116f03dfed10dea2dbfba3a88b3230ff1b62dd3e
okechpj <okechpj@example.com>
2025-06-02 00:43:31 +0300

    hello

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello from GitHub Actions + Flask!'
+    return 'Hello from GitHub Actions + Flask! and the entire community'
 
 if __name__ == '__main__':
     app.run(debug=True)
